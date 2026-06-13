e5b913e0212fe9a8b5b3a390e4fdce0dadc70e7f
123-SHIVA <shivayojha8@gmail.com>
2026-06-13 13:39:19 +0530

    'push'

--- src/components/Loader2.jsx
+++ src/components/Loader2.jsx
@@ -1,46 +1,7 @@
-// import React from "react";
-// import Spinner from "react-bootstrap/Spinner";
-
-// function Loader2({ visible }) {
-//   if (!visible) return null;
-
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         alignItems: "center",
-//         justifyContent: "center",
-//         position: "fixed", // ✅ use fixed to cover entire page
-//         top: 0,
-//         left: 0,
-//         width: "100vw",
-//         height: "100vh",
-//         // backgroundColor: "rgba(255, 255, 255, 0.2)", // lighter subtle overlay
-//         backgroundColor: "transparent",
-//         backdropFilter: "blur(2px)", // light glassmorphism blur
-//         WebkitBackdropFilter: "blur(2px)", // Safari support
-//         pointerEvents: "none", // allows clicking and scrolling through the loader (e.g. to access footer)
-//         zIndex: 9999, // make sure it's on top of everything
-//       }}
-//     >
-//       <Spinner
-//         animation="border"
-//         role="status"
-//         style={{ width: "3rem", height: "3rem", color: "#3A4B4C" }}
-//       >
-//         <span className="visually-hidden">Loading...</span>
-//       </Spinner>
-//     </div>
-//   );
-// }
-
-// export default React.memo(Loader2);
-
 import React from "react";
 import Spinner from "react-bootstrap/Spinner";
-import Button from "react-bootstrap/Button";
 
-function Loader({ visible }) {
+function Loader2({ visible }) {
   if (!visible) return null;
 
   return (
@@ -49,30 +10,28 @@ function Loader({ visible }) {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        position: "absolute",
+        position: "fixed", // ✅ use fixed to cover entire page
         top: 0,
         left: 0,
-        width: "100%",
-        height: "100%",
-        // backgroundColor: "rgba(255, 255, 255, 0.8)", 
+        width: "100vw",
+        height: "100vh",
+        // backgroundColor: "rgba(255, 255, 255, 0.2)", // lighter subtle overlay
         backgroundColor: "transparent",
-        zIndex: 1050,
+        backdropFilter: "blur(2px)", // light glassmorphism blur
+        WebkitBackdropFilter: "blur(2px)", // Safari support
+        pointerEvents: "none", // allows clicking and scrolling through the loader (e.g. to access footer)
+        zIndex: 9999, // make sure it's on top of everything
       }}
     >
-      <Button variant="primary" disabled>
-        <Spinner
-          as="span"
-          animation="border"
-          size="sm"
-          role="status"
-          aria-hidden="true"
-          variant="warning"
-        />
+      <Spinner
+        animation="border"
+        role="status"
+        style={{ width: "3rem", height: "3rem", color: "#3A4B4C" }}
+      >
         <span className="visually-hidden">Loading...</span>
-      </Button>
+      </Spinner>
     </div>
   );
 }
 
-export default React.memo(Loader);
-
+export default React.memo(Loader2);
